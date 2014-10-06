825ec655bd02a3c0223a297b5f71e8c2fabe2c09
BaitaCode <system.baitacode@gmail.com>
2014-10-05 23:09:52 -0300

    Adicionado a wrapper

--- footer.php
+++ footer.php
@@ -1,45 +1,47 @@
         <footer>
-        	<div class="left">
-        		 <h1>Baita Code</h1>
-            	 <h2>Desenvolvimento, Marketing e Chimarrão</h2>
-        	</div>
-        	<div class="rigth">
-        		<nav>
-	                <ul>
-	                    <li><a href="#" title="Ir para parte inicial do site">inicio</a></li>
-	                    <li><a href="#" title="Ir para area de serviços">serviços</a></li>
-	                    <li><a href="#" title="ir para area de portifólio">portifólio</a></li>
-	                    <li><a href="#" title="Entrar em contato conosco">contato</a></li>
-	                </ul>
-	            </nav>
-	            <address>
-	            	<ul>
-	            		<li>
-	            			Pelotas /RS <br />
-	            			Brasil <br />
-	            			Desde 2014
-	            		</li>
-	            		<li>
-	            			<li>contato@baitacode.com</li>
-	            			<li><a href="#" class="trabalhe-conosco" title="Trabalhe Conosco">Trabalhe Conosco</a></li>
-	            		</li>
-	            		<li>
-	            			Redes Sociais<br />
-	            			<div class="redes-sociais">
-	            				<a class="bt-twitter" href="#" title="Visitar nosso twitter">Twitter</a>
-	            				<a class="bt-facebook" href="#" title="Visitar nosso facebook">Facebook</a>
-	            				<a class="bt-linkedin" href="#" title="Visitar nosso linked-in">Linked-In</a>
-	            				<a class="bt-github" href="#" title="Visitar nosso github">Github</a>
-	            			</div>
-	            		</li>
-	            	</ul>
-	            	<nav class="idiomas">
+        	<div class="wrapper">
+	        	<div class="left">
+	        		 <h1>Baita Code</h1>
+	            	 <h2>Desenvolvimento, Marketing e Chimarrão</h2>
+	        	</div>
+	        	<div class="rigth">
+	        		<nav>
 		                <ul>
-		                    <li><a href="#" class="bt-pt-br" title="Alterar idioma para português brasil">Português Brasil</a></li>
-		                    <li><a href="#" class="bt-en" title="Alterar idioma para inglês EUA">English</a></li>
+		                    <li><a href="#" title="Ir para parte inicial do site">inicio</a></li>
+		                    <li><a href="#" title="Ir para area de serviços">serviços</a></li>
+		                    <li><a href="#" title="ir para area de portifólio">portifólio</a></li>
+		                    <li><a href="#" title="Entrar em contato conosco">contato</a></li>
 		                </ul>
 		            </nav>
-	            </address>
+		            <address>
+		            	<ul>
+		            		<li>
+		            			Pelotas /RS <br />
+		            			Brasil <br />
+		            			Desde 2014
+		            		</li>
+		            		<li>
+		            			<li>contato@baitacode.com</li>
+		            			<li><a href="#" class="trabalhe-conosco" title="Trabalhe Conosco">Trabalhe Conosco</a></li>
+		            		</li>
+		            		<li>
+		            			Redes Sociais<br />
+		            			<div class="redes-sociais">
+		            				<a class="bt-twitter" href="#" title="Visitar nosso twitter">Twitter</a>
+		            				<a class="bt-facebook" href="#" title="Visitar nosso facebook">Facebook</a>
+		            				<a class="bt-linkedin" href="#" title="Visitar nosso linked-in">Linked-In</a>
+		            				<a class="bt-github" href="#" title="Visitar nosso github">Github</a>
+		            			</div>
+		            		</li>
+		            	</ul>
+		            	<nav class="idiomas">
+			                <ul>
+			                    <li><a href="#" class="bt-pt-br" title="Alterar idioma para português brasil">Português Brasil</a></li>
+			                    <li><a href="#" class="bt-en" title="Alterar idioma para inglês EUA">English</a></li>
+			                </ul>
+			            </nav>
+		            </address>
+	        	</div>
         	</div>
         </footer>
 
